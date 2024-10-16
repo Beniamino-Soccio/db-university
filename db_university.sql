dff860b9d46c06f6083f2ba51d9d3d8a3bbb8757
Beniamino-Soccio <beniamino43@gmail.com>
2024-10-16 17:41:09 +0200

    end of exercise

---
+++ db_university.sql
@@ -0,0 +1,57 @@
+--Selezionare tutti gli studenti nati nel 1990 (160)
+SELECT * 
+FROM `students`
+WHERE YEAR(`date_of_birth`) = 1990;
+
+--Selezionare tutti i corsi che valgono più di 10 crediti (479)
+SELECT * 
+FROM `courses`
+WHERE `cfu` > 10;
+
+--Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT * 
+FROM `students`
+WHERE YEAR(`date_of_birth`) < 1994-10-16;
+
+--Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT * 
+FROM `courses`
+WHERE `period` = "I semestre"
+AND `year` = 1;
+
+--Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+SELECT *
+FROM `exams`
+WHERE DATE(`date`) = "2020-06-20";--79
+
+SELECT *
+FROM `exams`
+WHERE DATE(`date`) = "2020-06-20"
+AND HOUR(`hour`) > "14:00:00";--14 (sbagliata)
+
+--Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * 
+FROM `degrees` 
+WHERE `level` = "magistrale";
+
+--Da quanti dipartimenti è composta l'università? (12)
+SELECT COUNT(*)
+FROM `departments`;
+
+--Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT * 
+FROM `teachers` 
+WHERE `phone` IS NULL;
+
+--Inserire nella tabella degli studenti un nuovo record con i propri dati (per il campo degree_id, inserire un valore casuale)
+INSERT INTO `students` (`id`, `degree_id`, `name`, `surname`, `date_of_birth`, `fiscal_code`, `enrolment_date`, `registration_number`, `email`) 
+VALUES ('5001', '29', 'Beniamino', 'Soccio', '1998-12-24', 'DAJE1234DAJE5678', '2020-02-18', '999999', 'dajedaje@gmail.com')
+
+--Cambiare il numero dell’ufficio del professor Pietro Rizzo in 126
+SELECT `office_number` 
+FROM `teachers` 
+WHERE REPLACE('125', '5', '6'); --(sicuramente sbagliata)
+
+--Eliminare dalla tabella studenti il record creato precedentemente al punto 9
+DELETE FROM students 
+WHERE `students`.`id` = 5001
